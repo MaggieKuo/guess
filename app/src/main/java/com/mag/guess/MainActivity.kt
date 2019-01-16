b066cf9c3763a4d3c3a058a2281a872f7de09a76
MaggieKuo <perfumekuo@gmail.com>
2019-01-16 21:21:13 +0800

    mutableListOf, when(n) {

--- app/src/main/java/com/mag/guess/MainActivity.kt
+++ app/src/main/java/com/mag/guess/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.mag.guess
 
 import android.content.Context
-import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,22 +19,15 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             playMedia()
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
         }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -56,79 +42,70 @@ class MainActivity : AppCompatActivity() {
         speechPlayer.queue(87)
         speechPlayer.play()
 
-//        var audios = R.raw::class.java.fields
-//        audios.map { println(it.name) }
-
-/*
-        var media: Array<MediaPlayer>
-        media = arrayOf(MediaPlayer.create(applicationContext, R.raw.n1),
-            MediaPlayer.create(applicationContext, R.raw.n1),
-            MediaPlayer.create(applicationContext, R.raw.n3))
-        media.forEach { it.start() }
-*/
     }
 
     class SpeechPlayer(val context: Context) : MediaPlayer.OnCompletionListener {
+        lateinit var mp3: MutableMap<String, MediaPlayer>
+        init {
+            mp3 = Audios.getInstance(context)
+        }
+
         override fun onCompletion(mp: MediaPlayer?) {
             play()
         }
 
-        val speech = ArrayList<Int>()
+        val speech = mutableListOf<MediaPlayer>()
         fun queue(n: Any) {
             val medias = when (n) {
                 is Int -> numberMedia(n)
-                else -> wordMedia(n as String)
+                else -> addMedia(n as String)
             }
         }
 
         fun play(){
             if (speech.size>0){
-                val mediaPlayer = MediaPlayer.create(context, speech.get(0))
-                speech.removeAt(0)
+                speech.get(0).start()
 //                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    mediaPlayer.playbackParams = mediaPlayer.playbackParams.setSpeed(1f)
+//                    speech.get(0).playbackParams = speech.get(0).playbackParams.setSpeed(15f)
 //                }
-                mediaPlayer.setOnCompletionListener(this)
-                mediaPlayer.start()
+                speech.removeAt(0)
             }
         }
 
-        private fun wordMedia(n: String){
-            speech.add(Audios.getInstance(context).get("to")!!)
+        private fun addMedia(key: String){
+            var mp = mp3.get(key)
+            mp!!.setOnCompletionListener(this)
+            speech.add(mp)
         }
 
         private fun numberMedia(n: Int) {
-            val audioMap = Audios.getInstance(context)
             when(n){
-                in 11..19 -> {
-                    speech.add(audioMap.get("n10")!!)
-                    speech.add(audioMap.get("n" + (n%10))!!)
+                in 0..10 -> addMedia("n" + n)
+                in 11..99 -> {
+                    if (n/10 > 1) addMedia("n" + (n/10))
+                    addMedia("n10")
+                    if (n%10 > 0) addMedia("n" + (n%10))
                 }
-                in 20..99 -> {
-                    speech.add(audioMap.get("n" + (n/10))!!)
-                    speech.add(audioMap.get("n10")!!)
-                    if (n%10 > 0){
-                        speech.add(audioMap.get("n" + (n%10))!!)
-                    }
-                }
-                in 0..10 -> speech.add(audioMap.get("n" + n)!!)
             }
         }
 
-        private fun getRawId(name: String)= Audios.getInstance(context).get(name)
 
     }
 
+
     class Audios() {
         companion object {
-            private var instance = HashMap<String, Int>()
+            private var instance = mutableMapOf<String, MediaPlayer>()
             fun getInstance(context: Context) = if (instance.size > 0) instance!!
             else {
                 for (field in R.raw::class.java.fields) {
-                    instance.put(field.name, context.resources.getIdentifier(field.name, "raw", context.packageName))
+                    instance.put(field.name, MediaPlayer.create(context,
+                        context.resources.getIdentifier(field.name, "raw", context.packageName)))
                 }
                 instance!!
             }
+
+
         }
     }
 
