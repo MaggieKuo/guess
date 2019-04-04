50b39d352f4fbd8f7ca2b7c40ff25aa2e766cfb2
MaggieKuo <perfumekuo@gmail.com>
2019-04-05 00:17:59 +0800

    .

--- app/src/main/java/com/mag/guess/MainActivity.kt
+++ app/src/main/java/com/mag/guess/MainActivity.kt
@@ -2,24 +2,29 @@ package com.mag.guess
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.media.PlaybackParams
 import android.os.Build
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
+
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var secret:Int = 0
+    var lastNumber: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            playMedia()
-        }
+//        fab.setOnClickListener { _view ->
+//
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,20 +39,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun playMedia() {
+    fun reset(){
+        secret = Random.nextInt(100)
+        println(secret)
+    }
 
+    fun guess(){
+        val num = et_secret.text.toString().toInt()
+        when(num){
+            secret -> playMedia("corrected")
+//            in lastNumber..secret -> playMedia(num, 100)
+            in 0..secret -> playMedia(num, minOf(lastNumber, 100))
+            in secret..100 -> playMedia(maxOf(0, lastNumber), num)
+            else -> playMedia("again")
+        }
+        lastNumber =  if (num>secret && num>lastNumber) lastNumber else num
+    }
+
+    fun playMedia(start: Int, end: Int) {
         var speechPlayer = SpeechPlayer(this)
-        speechPlayer.queue(11)
+        speechPlayer.queue(start)
         speechPlayer.queue("to")
-        speechPlayer.queue(87)
+        speechPlayer.queue(end)
         speechPlayer.play()
+    }
 
+    fun playMedia(media: String){
+        var speechPlayer = SpeechPlayer(this)
+        speechPlayer.queue(media)
+        speechPlayer.play()
     }
 
     class SpeechPlayer(val context: Context) : MediaPlayer.OnCompletionListener {
-        lateinit var mp3: MutableMap<String, MediaPlayer>
+        var mp3: MutableMap<String, MediaPlayer>
+        var params: PlaybackParams? = null
+
         init {
             mp3 = Audios.getInstance(context)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                params = PlaybackParams().apply {
+                    speed = 2f
+                    pitch = 1f
+                    audioFallbackMode = PlaybackParams.AUDIO_FALLBACK_MODE_DEFAULT
+                }
+
+            }
         }
 
         override fun onCompletion(mp: MediaPlayer?) {
@@ -55,18 +91,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         val speech = mutableListOf<MediaPlayer>()
-        fun queue(n: Any) {
-            val medias = when (n) {
+        fun queue(n: Any) = when (n) {
                 is Int -> numberMedia(n)
                 else -> addMedia(n as String)
-            }
         }
 
         fun play(){
             if (speech.size>0){
                 speech.get(0).start()
 //                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    speech.get(0).playbackParams = speech.get(0).playbackParams.setSpeed(15f)
+//                    speech.get(0).params = speech.get(0).params.setSpeed(15f)
 //                }
                 speech.removeAt(0)
             }
@@ -74,6 +108,9 @@ class MainActivity : AppCompatActivity() {
 
         private fun addMedia(key: String){
             var mp = mp3.get(key)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//               mp!!.playbackParams = params
+            }
             mp!!.setOnCompletionListener(this)
             speech.add(mp)
         }
@@ -86,29 +123,27 @@ class MainActivity : AppCompatActivity() {
                     addMedia("n10")
                     if (n%10 > 0) addMedia("n" + (n%10))
                 }
+                100 -> {
+                    addMedia("n1")
+                    addMedia("hundred")
+                }
             }
         }
-
-
     }
 
 
     class Audios() {
         companion object {
             private var instance = mutableMapOf<String, MediaPlayer>()
-            fun getInstance(context: Context) = if (instance.size > 0) instance!!
+            fun getInstance(context: Context) = if (instance.size > 0) instance
             else {
                 for (field in R.raw::class.java.fields) {
                     instance.put(field.name, MediaPlayer.create(context,
                         context.resources.getIdentifier(field.name, "raw", context.packageName)))
                 }
-                instance!!
+                instance
             }
-
-
         }
     }
-
-
 }
 
